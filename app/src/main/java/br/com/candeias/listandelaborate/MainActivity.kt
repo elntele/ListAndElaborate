d015534cfc33bbc5dc8028337bf31b94f02cea30
elntele <elntele@gmail.com>
2023-09-09 00:36:37 -0300

    flavors criated, firebase configured, still need implementation for logi firebase

---
+++ app/src/main/java/br/com/candeias/listandelaborate/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.candeias.listandelaborate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.listandelaborate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         binding= ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
