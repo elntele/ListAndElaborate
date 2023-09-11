e6ff27b08a8b6bf082badcc2eebcb425ee01e3eb
elntele <elntele@gmail.com>
2023-09-10 23:26:30 -0300

    add login screen

---
+++ app/src/main/java/br/com/candeias/listandelaborate/beginscreem/ItinitialScreen.kt
@@ -0,0 +1,39 @@
+package br.com.candeias.listandelaborate.beginscreem
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.candeias.listandelaborate.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ItinitialScreen.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ItinitialScreen : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+         /*   param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)*/
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_itinitial_screen, container, false)
+    }
+
+    companion object {
+
+    }
+}
